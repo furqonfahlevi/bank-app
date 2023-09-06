8dc7a50d3378cfc0db0051a9a91accc8d961989a
furqonfahlevi <furqonfahlevirm@gmail.com>
2023-09-06 11:06:20 +0700

    add label component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
@@ -26,17 +27,21 @@ export default function Home() {
         </SelectContent>
       </Select> */}
       {/* <div>
+        <Label htmlFor="name">Customer name</Label>
         <Input type="name" id="name" placeholder="Type customer name"></Input>
+        <Label htmlFor="name">Customer email</Label>
         <Input
           type="email"
           id="email"
           placeholder="Type customer email"
         ></Input>
+        <Label htmlFor="name">Item name</Label>
         <Input
           type="item"
           id="item"
           placeholder="Type customer item name"
         ></Input>
+        <Label htmlFor="name">Item mount</Label>
         <Input type="mount" id="mount"></Input>
       </div> */}
     </div>

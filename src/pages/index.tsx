a97e5a448f32c2f67936bab483c943b89ae47dcd
furqonfahlevi <furqonfahlevirm@gmail.com>
2023-09-06 10:25:27 +0700

    add select component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Button variant={"ghost"}>ini tombol 1</Button>
-      <Button>ini tombol 2</Button>
+      {/* <Button variant={"ghost"}>ini tombol 1</Button>
+      <Button>ini tombol 2</Button> */}
+      {/* <Select defaultValue="monthly">
+        <SelectTrigger>
+          <SelectValue></SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="daily">Daily</SelectItem>
+          <SelectItem value="weekly">Weekly</SelectItem>
+          <SelectItem value="monthly">Monthly</SelectItem>
+          <SelectItem value="yearly">Yearly</SelectItem>
+        </SelectContent>
+      </Select> */}
     </div>
   );
 }

4aa4782f1eb30612790512b6588fc7f302c64683
furqonfahlevi <furqonfahlevirm@gmail.com>
2023-09-06 09:53:21 +0700

    changes button style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Button>ini tombol</Button>
+      <Button variant={"ghost"}>ini tombol 1</Button>
+      <Button>ini tombol 2</Button>
     </div>
   );
 }

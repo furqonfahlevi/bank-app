ce5259fd32477e71a3ccd937b36e065bbf5ef202
furqonfahlevi <furqonfahlevirm@gmail.com>
2023-09-06 14:22:35 +0700

    fix menu component

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -5,9 +5,9 @@ export default function Menu() {
   return (
     <div>
       <Link href={"/"}>
-        <div className="w-full h-20 pl-7 pr-5 py-5 bg-white flex-col justify-center items-center gap-5 inline-flex hover:cursor-pointer hover:bg-accent">
-          <div className="w-[232px] h-12 relative border-r-[3.27px] border-primary">
-            <div className="w-36 h-6 left-0 top-[12px] absolute justify-start items-center gap-4 inline-flex">
+        <div className="w-full h-20 pl-7 pr-5 py-5 bg-white flex-col justify-center items-center gap-5 inline-flex hover:cursor-pointer hover:bg-accent border-r-[3.27px] border-primary">
+          <div className="w-full h-6 justify-between items-center gap-4 inline-flex">
+            <div className="flex gap-4">
               <div className="w-6 h-6 justify-center items-center flex">
                 <Category />
               </div>
